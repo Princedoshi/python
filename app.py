0b2e9ba5211911b3dd4e9c20a4f2e82cff6224b9
Prince  Doshi <Prince@Princes-MacBook-Air.local>
2024-02-01 02:57:46 +0530

    /

--- app.py
+++ app.py
@@ -15,43 +15,43 @@ headers = {"Authorization": "Bearer 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"}
 def hello():
     return "Hello, World!"
 
-@app.route('/process_audio', methods=['POST'])
-def process_audio():
-    try:
-        audio_data = request.files['audio'].read()
-        audio_text = recognize_audio(audio_data)
-        text=summarize_text(audio_text)
+# @app.route('/process_audio', methods=['POST'])
+# def process_audio():
+#     try:
+#         audio_data = request.files['audio'].read()
+#         audio_text = recognize_audio(audio_data)
+#         text=summarize_text(audio_text)
         
-        return jsonify({'text': text,'ogtext':audio_text,"keywords":query({"inputs": text,})})
-    except Exception as e:
-        return jsonify({'error': str(e)})
+#         return jsonify({'text': text,'ogtext':audio_text,"keywords":query({"inputs": text,})})
+#     except Exception as e:
+#         return jsonify({'error': str(e)})
     
-def recognize_audio(audio_data):
-    response = requests.post(API_URL, headers=headers, data=audio_data)
-    # print(response.json()['text'])
-    return (response.json()['text'])
+# def recognize_audio(audio_data):
+#     response = requests.post(API_URL, headers=headers, data=audio_data)
+#     # print(response.json()['text'])
+#     return (response.json()['text'])
 
-# app = Flask(_name_)
+# # app = Flask(_name_)
 
-API2_URL = "https://api-inference.huggingface.co/models/transformer3/H1-keywordextractor"
-headers2 = {"Authorization": "Bearer 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"}
+# API2_URL = "https://api-inference.huggingface.co/models/transformer3/H1-keywordextractor"
+# headers2 = {"Authorization": "Bearer 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"}
 
-def query(payload):
-	response = requests.post(API2_URL, headers=headers2, json=payload)
-	return [item.strip() for item in response.json()[0].get('summary_text', '').split(',')]
+# def query(payload):
+# 	response = requests.post(API2_URL, headers=headers2, json=payload)
+# 	return [item.strip() for item in response.json()[0].get('summary_text', '').split(',')]
 
-# @app.route('/summarize', methods=['POST'])
-def summarize_text(article):
-    try:
-        # if not request.is_json:
-        #     return jsonify({'error': 'Invalid request. Please provide a JSON payload.'})
-        # data = request.get_json()
-        # article = data['text']
-        summary = summarizer(article, max_length=300,
-                             min_length=100, do_sample=False)
-        return summary[0]['summary_text']
+# # @app.route('/summarize', methods=['POST'])
+# def summarize_text(article):
+#     try:
+#         # if not request.is_json:
+#         #     return jsonify({'error': 'Invalid request. Please provide a JSON payload.'})
+#         # data = request.get_json()
+#         # article = data['text']
+#         summary = summarizer(article, max_length=300,
+#                              min_length=100, do_sample=False)
+#         return summary[0]['summary_text']
 
-    except Exception as e:
-        print("hi")
-        return jsonify({'error': str(e)})
+    # except Exception as e:
+    #     print("hi")
+    #     return jsonify({'error': str(e)})
     
