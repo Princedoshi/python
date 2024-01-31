a6c146d3191c6045996284b30813926fb102ee1f
Prince  Doshi <Prince@Princes-MacBook-Air.local>
2024-02-01 02:33:52 +0530

    /

--- a.py
+++
@@ -1,7 +0,0 @@
-import pkg_resources
-
-try:
-    version = pkg_resources.get_distribution("pydub").version
-    print("pydub version:", version)
-except pkg_resources.DistributionNotFound:
-    print("pydub is not installed")
